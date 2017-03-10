4c8d9255efbdfa6adcebebba3e8c119822438c09
Tyler Straub <tyler.straub@gmail.com>
2017-03-09 18:30:16 -0800

    converted metadata polling to webworker
    WEB WORKERS ARE FUCKING AWESOME

--- radio.js
+++ radio.js
@@ -3,95 +3,88 @@ var throttleHornInputs = false;
 var bearhorns = [];
 // load the bearhorns
 var bearhorn1 = new Howl({
-    src: ['./sounds/bearhorn1.mp3']
-    , volume: 0.5
-    , preload: true
+	src: ['./sounds/bearhorn1.mp3']
+	, volume: 0.5
+	, preload: true
 });
 var bearhorn2 = new Howl({
-    src: ['./sounds/bearhorn2.mp3']
-    , volume: 0.5
-    , preload: true
+	src: ['./sounds/bearhorn2.mp3']
+	, volume: 0.5
+	, preload: true
 });
 var bearhorn3 = new Howl({
-    src: ['./sounds/bearhorn3.mp3']
-    , volume: 0.5
-    , preload: true
+	src: ['./sounds/bearhorn3.mp3']
+	, volume: 0.5
+	, preload: true
 });
 var bearhorn4 = new Howl({
-    src: ['./sounds/bearhorn4.mp3']
-    , volume: 0.5
-    , preload: true
+	src: ['./sounds/bearhorn4.mp3']
+	, volume: 0.5
+	, preload: true
 });
 bearhorns = [bearhorn1, bearhorn1, bearhorn3, bearhorn4];
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
 }
 // sweet color generator function lulz
 function getRandomColor() {
-    var color = ["yellowgreen", "#ff33cc"];
-    var i = getRandomInt(0, color.length);
-    return color[i];
+	var color = ["yellowgreen", "#ff33cc"];
+	var i = getRandomInt(0, color.length);
+	return color[i];
 }
 // arm the bearhorn...
 function blowHorn(randomColor) {
-    if (!throttleHornInputs) {
-        var whichHorn = getRandomInt(0, 4); // array positions 0-3
-        var thisHorn = bearhorns[whichHorn];
-        thisHorn.play();
-        throttleHornInputs = true;
-        var backgroundBlink = setInterval(function () {
-            var currentBGcolor = $(".centerContainer").css("background-color");
-            if (currentBGcolor === "rgba(0, 0, 0, 0)") {
-                $(".centerContainer").css("background-color", randomColor);
-                if (randomColor == "#ff33cc") {
-                    $("#boh").css("color", "yellowgreen");;
-                }
-                else {
-                    $("#boh").css("color", "#ff33cc");
-                }
-                $("#boh").show();
-            }
-            else {
-                $(".centerContainer").css("background-color", "transparent");
-                $("#boh").hide();
-            }
-        }, 69);
-        // CONTINUE ALL THE ABOVE STATES FOR 800 MS
-        setTimeout(function () {
-            // SIGNAL THE STOP
-            throttleHornInputs = false;
-            // STOP THE ANIMATION
-            // STOP THE BLINKING
-            clearInterval(backgroundBlink);
-            // STOP THE OTHER STUFF
-            $(".centerContainer").css("background-color", "transparent");
-            $("#boh").hide();
-        }, 800);
-        // INCREASE HYPE
-        $("#hornCount").text(parseInt($("#hornCount").text()) + 1);
-    }
-}
-// get the track ID
-function getTrackID() {
-    $.getJSON('http://listen.seriousbears.net/stats', function (data) {
-        console.log(data.icestats.source[0].title);
-        $("#nowplaying").replaceWith(data.icestats.source[0].title);
-    });
+	if (!throttleHornInputs) {
+		var whichHorn = getRandomInt(0, 4); // array positions 0-3
+		var thisHorn = bearhorns[whichHorn];
+		thisHorn.play();
+		throttleHornInputs = true;
+		var backgroundBlink = setInterval(function () {
+			var currentBGcolor = $(".centerContainer").css("background-color");
+			if (currentBGcolor === "rgba(0, 0, 0, 0)") {
+				$(".centerContainer").css("background-color", randomColor);
+				if (randomColor == "#ff33cc") {
+					$("#boh").css("color", "yellowgreen");;
+				}
+				else {
+					$("#boh").css("color", "#ff33cc");
+				}
+				$("#boh").show();
+			}
+			else {
+				$(".centerContainer").css("background-color", "transparent");
+				$("#boh").hide();
+			}
+		}, 69);
+		// CONTINUE ALL THE ABOVE STATES FOR 800 MS
+		setTimeout(function () {
+			// SIGNAL THE STOP
+			throttleHornInputs = false;
+			// STOP THE ANIMATION
+			// STOP THE BLINKING
+			clearInterval(backgroundBlink);
+			// STOP THE OTHER STUFF
+			$(".centerContainer").css("background-color", "transparent");
+			$("#boh").hide();
+		}, 800);
+		// INCREASE HYPE
+		$("#hornCount").text(parseInt($("#hornCount").text()) + 1);
+	}
 }
 // this function will control which media module at the top is currently active. 
 function displayModule(module) {
-    var twitch = "none";
-    var oso = "none";
-    if (module == "twitch") {
-        twitch = "block";
-    }
-    if (module == "oso") {
-        oso = "block";
-    }
-    document.getElementById("twitchContainer").style.display = twitch;
-    document.getElementById("muteNotice").style.display = twitch;
-    document.getElementById("osoContainer").style.display = oso;
+	var twitch = "none";
+	var oso = "none";
+	if (module == "twitch") {
+		twitch = "block";
+	}
+	if (module == "oso") {
+		oso = "block";
+	}
+	document.getElementById("twitchContainer").style.display = twitch;
+	document.getElementById("muteNotice").style.display = twitch;
+	document.getElementById("osoContainer").style.display = oso;
 }
---
+++ trackidworker.js
@@ -0,0 +1,20 @@
+// RECURSIVE POLL FOR STREAM METADATA WRAPPED INTO A WEB WORKER
+function getTrackID() {
+	setInterval(function () {
+		console.log("Web worker is now polling for metadata...");
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', "//listen.seriousbears.net/stats", true);
+		xhr.send();
+		xhr.addEventListener("readystatechange", processRequest, false);
+
+		function processRequest(e) {
+			if (xhr.readyState == 4 && xhr.status == 200) {
+				var response = JSON.parse(xhr.responseText);
+				// send back the string through the webworker thread
+				console.log("NP: " + response.icestats.source[0].title);
+				postMessage(response.icestats.source[0].title);
+			}
+		}
+	}, 15000);
+}
+getTrackID();
